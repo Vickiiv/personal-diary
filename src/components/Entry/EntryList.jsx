815c29c3713d4632637cd7973da145354aa62798
Vickiiv <virzivictoria@gmail.com>
2026-06-11 11:24:38 +0200

    update entrylist layout

--- src/components/Entry/EntryList.jsx
+++ src/components/Entry/EntryList.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import EntryCard from "./EntryCard";
 
 function EntryList({ sortedEntries, deleteEntry }) {
+  if (sortedEntries.length === 0) {
+    return (
+      <div className="flex justify-center m-20">
+        <p className="">Keine Einträge vorhanden</p>
+      </div>
+    );
+  }
   return (
     <div className="my-20 mx-20 flex flex-col gap-2">
       {sortedEntries.map((entry) => (
