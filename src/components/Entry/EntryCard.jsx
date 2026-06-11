8e5f8509dab49705b678db1c5ad00f0d95e1708c
Vickiiv <virzivictoria@gmail.com>
2026-06-11 10:19:15 +0200

    add entry-details

--- src/components/Entry/EntryCard.jsx
+++ src/components/Entry/EntryCard.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 function EntryCard({ entry }) {
   const date = new Date(entry.date);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const expandSwitch = () => setIsExpanded((isExpanded) => !isExpanded);
 
   const formattedDate = `${String(date.getDate()).padStart(2, "0")}.${date.toLocaleString(
     "de-DE",
@@ -20,19 +22,28 @@ function EntryCard({ entry }) {
       )}
       <p className="font-mono">{formattedDate}</p>
       <h2 className="font-mono text-xl font-bold">{entry.title}</h2>
-
-      <div className="border-t-2 py-4">
-        <p className="break-normal whitespace-pre-wrap">{entry.content}</p>
-      </div>
-
-      <div className="flex justify-end border-t-2 pt-2">
-        <button className="cursor-pointer hover:bg-gray-200 rounded-xl px-2 py-1 ">
-          Bearbeiten
-        </button>
-        <button className="cursor-pointer hover:bg-gray-200 rounded-xl px-2 py-1 ">
-          Löschen
+      <div className="flex justify-end">
+        <button onClick={expandSwitch} className="bg-pink-200 cursor-pointer">
+          {isExpanded ? "" : ""}
         </button>
       </div>
+
+      {isExpanded && (
+        <div>
+          <div className="border-t-2 py-4">
+            <p className="break-normal whitespace-pre-wrap">{entry.content}</p>
+          </div>
+
+          <div className="flex justify-end border-t-2 pt-2">
+            <button className="cursor-pointer hover:bg-gray-200 rounded-xl px-2 py-1 ">
+              Bearbeiten
+            </button>
+            <button className="cursor-pointer hover:bg-gray-200 rounded-xl px-2 py-1 ">
+              Löschen
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
